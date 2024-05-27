809b0a0514477434f9ee1396f3c77b31a4074936
techtraaz <101785845+techtraaz@users.noreply.github.com>
2024-05-27 18:02:30 +0530

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,74 @@
+
+var item = document.getElementsByClassName('item');
+var lastitem = document.getElementsByClassName('last-item');
+
+
+function fadein1(){
+    item[0].style.opacity = 1;
+}
+
+setInterval(fadein1,1000);
+
+function fadein2(){
+    item[1].style.opacity = 1;
+}
+
+setInterval(fadein2,2000);
+
+function fadein3(){
+    item[2].style.opacity = 1;
+}
+
+setInterval(fadein3,3000);
+
+function fadein4(){
+    item[3].style.opacity = 1;
+}
+
+setInterval(fadein4,4000);
+
+function fadein5(){
+    item[4].style.opacity = 1;
+}
+
+setInterval(fadein5,5000);
+
+
+function lastfadein5(){
+    lastitem[4].style.opacity = 1;
+}
+
+setInterval(lastfadein5,5000);
+
+
+function lastfadein4(){
+    lastitem[3].style.opacity = 1;
+}
+
+setInterval(lastfadein4,6000);
+
+function lastfadein6(){
+    lastitem[2].style.opacity = 1;
+}
+
+setInterval(lastfadein6,7000);
+
+function lastfadein7(){
+    lastitem[1].style.opacity = 1;
+}
+
+setInterval(lastfadein7,8000);
+
+
+function lastfadein8(){
+    lastitem[0].style.opacity = 1;
+}
+
+setInterval(lastfadein8,9000);
+
+
+
+
+
+
+
